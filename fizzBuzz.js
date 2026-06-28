0c9e76a9138e728566ed200484e4d80400146c36
Karla G. <k.grajalesluna@gmail.com>
2026-06-28 23:53:12 +0100

    refactor: enhance FizzBuzz function with detailed comments and test cases

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -24,20 +24,50 @@
 // 1 <= n <= 104
 
 
-let FizzBuzz = function(n){
-    let result = [];
+let fizzBuzz = function (n) {
+  //we declared a result variable to store the final result which is an array of strings
+  let result = [];
 
-    for(let i = 1; i <= n; i++){
-
-        if(i % 3 === 0 && i % 5 === 0 ){
-            result.push("FizzBuzz")
-        } else if (i % 3 === 0){
-            result.push("Fizz")
-        } else if (i % 5 === 0){
-            result.push("Buzz")
-        } else {
-            result.push(i.toString())
-        }
+  //this loop start from 1 because the problem is 1-indexed and it will loop until n
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      //everything that is divisible by 3 and 5 will be pushed to the result array as "FizzBuzz" to the end of the array
+      result.push('FizzBuzz');
+    } else if (i % 3 === 0) {
+      result.push('Fizz');
+    } else if (i % 5 === 0) {
+      result.push('Buzz');
+    } else {
+      //if the number is not divisible by 3 or 5, we will push the number as a string to the result array
+      result.push(i.toString());
     }
-    return result;
+  }
+  return result;
 };
+
+
+//Manually test cases:
+
+
+//this will be an array of test cases where each test case is an array of two elements, the first element is the input n and the second element is the expected output array
+const testCases = [
+  [3, ['1', '2', 'Fizz']],
+  [5, ['1', '2', 'Fizz', '4', 'Buzz']],
+  [15, [ '1', '2', 'Fizz', '4', 'Buzz', 'Fizz', '7', '8', 'Fizz','Buzz', '11', 'Fizz', '13', '14', 'FizzBuzz']]
+];
+
+console.log('-------------------- test cases --------------------');
+
+testCases.forEach(([input, expected]) => {
+  const result = fizzBuzz(input);
+
+  const passed = result.join(',') === expected.join(',');
+
+  console.log(`Input: ${input} Expected ${expected} | Test Passed: ${passed}`);
+
+  for (let i = 0; i < input; i++) {
+    if (result.at(i) === expected.at(i)) {
+      console.log(`Elements at ${i} is equal`);
+    }
+  }
+});
